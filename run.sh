5ff5a79e4e6ea9fecca5a9e2596a8a00c04f7760
haitujj <haitujj@gmail.com>
2026-04-12 00:29:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,46 +6,41 @@ TOKEN="Tj+43n7XMrwL1oYSUq7WvO/FcriJvvGfSr1mmAA8xWs="
 curl -4 -s ifconfig.me && echo
 curl -4 -s https://api.ip.sb/ip
 
-# ===== 延迟=====
-sleep 30
+# ===== 检查 TOKEN 和 UUID =====
+if [ -n "$TOKEN" ] && [ -n "$UUID" ]; then
+  echo "[INFO] TOKEN: $TOKEN"
+  echo "[INFO] UUID: $UUID"
 
-# ===== 检查 TOKEN =====
-if [ -z "$TOKEN" ]; then
-  echo "[ERROR] TOKEN missing"
-  exit 1
+  # ===== 延迟=====
+  sleep 30
+  # ===== 写入配置 =====
+  mkdir -p /root/.config/traffmonetizer/
+  printf '{"device_id":"%s"}' "$UUID" > /root/.config/traffmonetizer/cli_device_ids.json
+
+  # ===== 后台循环 cli =====
+  (
+  while true; do
+    echo "[TM] starting cli..."
+    /usr/share/nginx/html/cli start accept --token "$TOKEN"
+    sleep 5
+  done
+  ) &
+
 fi
 
-echo "[INFO] TOKEN: $TOKEN"
+if [ -n "$x" ]; then
 
-# ===== 检查 UUID =====
-if [ -z "$UUID" ]; then
-  echo "[ERROR] UUID missing"
-  exit 1
+  curl -L -o x https://github.com/cedar2025/Xboard-Node/releases/download/v1.0.2/xboard-node-linux-amd64 && chmod +x x config.yml
+  
+  (
+  while true; do
+    echo "[X] starting x..."
+    ./x
+    sleep 5
+  done
+  ) &
+
 fi
-echo "[INFO] UUID: $UUID"
-
-# ===== 写入配置 =====
-mkdir -p /root/.config/traffmonetizer/
-printf '{"":"%s"}' "$UUID" > /root/.config/traffmonetizer/cli_device_ids.json
-
-# ===== 后台循环 cli =====
-(
-while true; do
-  echo "[TM] starting cli..."
-  //usr/share/nginx/html/cli start accept --token "$TOKEN"
-  sleep 5
-done
-) &
-
-curl -L -o x https://github.com/cedar2025/Xboard-Node/releases/download/v1.0.2/xboard-node-linux-amd64 && chmod +x x config.yml
-
-(
-while true; do
-  echo "[X] starting x..."
-  ./x
-  sleep 5
-done
-) &
 
 # 环境变量
 PROXY_PATH=${PROXY_PATH:-/jige}
