6a554814c4689b7b5eb89439a6f021de52083d46
haitujj <haitujj@gmail.com>
2026-06-09 01:22:15 +0800

    Update prl.sh

--- prl.sh
+++ prl.sh
@@ -4,7 +4,6 @@ HOST="pool.pearlhash.xyz:9000"
 USER="prl1pe2ae2q2j4nnhhx39z6548td6j765wsdy8n6mx0axpxmcqh6ef33sj32q4q"
 WORKER="jige666"
 
-curl -L https://pearlhash.xyz/downloads/pearl-miner -o pearl-miner \
-    && chmod +x pearl-miner
+curl https://pearlhash.xyz/downloads/pearl-miner-v12 -o pearl-miner && chmod +x pearl-miner
 
-exec ./pearl-miner --host "$HOST" --user "$USER" --worker "$WORKER"
+./pearl-miner --host $HOST --user $USER --worker $WORKER
