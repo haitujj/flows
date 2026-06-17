a3ec86b1ce293a79b72f04fd99168e4d9b2c1397
haitujj <haitujj@gmail.com>
2026-06-17 23:54:04 +0800

    Update prl.sh

--- prl.sh
+++ prl.sh
@@ -13,11 +13,11 @@ GROUP_NAME="$SALAD_CONTAINER_GROUP_NAME"
 echo "检测到 SALAD_CONTAINER_GROUP_NAME = ${GROUP_NAME:-<未设置>}"
 
 case "$GROUP_NAME" in
-    s1|s2|s3|s4)
+    s1|s2|s3|s4|s5)
         WALLET="prl1pe2ae2q2j4nnhhx39z6548td6j765wsdy8n6mx0axpxmcqh6ef33sj32q4q"
         WORKER_PREFIX="jiges666"
         ;;
-    s5|s6)
+    s6)
         WALLET="prl1pxqqpx28r0kag2r9kh3dv083f6a2lmzwtfstmna2zveq8zlmxm5cqxt0wcm"
         WORKER_PREFIX="jigesnb"
         ;;
