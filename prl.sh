b9c469a869572407a759035a669b50f76b5f1b92
haitujj <haitujj@gmail.com>
2026-09-01 00:50:02 +0800

    Update prl.sh

--- prl.sh
+++ prl.sh
@@ -106,7 +106,7 @@ LAST_HASH_LINE=""
 
             echo "[$(date '+%Y-%m-%d %H:%M:%S')] No hashrate detected (${NO_HASH_COUNT}/10)"
 
-            if [ "$NO_HASH_COUNT" -ge 20 ]; then
+            if [ "$NO_HASH_COUNT" -ge 60 ]; then
 
                 echo "[$(date '+%Y-%m-%d %H:%M:%S')] No hashrate detected for 10 consecutive checks, sending reallocate request..."
                 exit 1
